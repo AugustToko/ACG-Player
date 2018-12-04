5a3aaf814843df3f47586c2c5e7a2b1d8419d74b
chenlongcould <827266641@qq.com>
2018-12-04 13:17:54 +0800

    1. 优化滚动性能 2. RecyclerItem优化
    Signed-off-by: chenlongcould <827266641@qq.com>

---
+++ app/src/main/java/top/geek_studio/chenlongcould/musicplayer/VisibleOrGone.java
@@ -0,0 +1,16 @@
+/*
+ * ************************************************************
+ * 文件：VisibleOrGone.java  模块：app  项目：MusicPlayer
+ * 当前修改时间：2018年12月04日 13:17:53
+ * 上次修改时间：2018年12月02日 11:05:21
+ * 作者：chenlongcould
+ * Geek Studio
+ * Copyright (c) 2018
+ * ************************************************************
+ */
+
+package top.geek_studio.chenlongcould.musicplayer;
+
+public interface VisibleOrGone {
+    void visibleOrGone(int status);
+}
